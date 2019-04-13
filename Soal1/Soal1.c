debc8a75f6f612ca7e8b4ce794e9f79ec8afadf1
Garda Negara W <45958288+gardanwm@users.noreply.github.com>
2019-04-13 19:13:27 +0700

    Update Soal1.c

--- Soal1/Soal1.c
+++ Soal1/Soal1.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
-#include<string.h>
 #include<pthread.h>
 #include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
 
+int fac[100];
+int hasil[100];
+int x=0;
 int sort(const void * a, const void * b) {
    return ( *(int*)a - *(int*)b );
 }
@@ -16,22 +15,33 @@ void* fakt(void *a){
 	while(an>0){
 	f=f*an; an=an-1;
 	}
-	printf("%d!= %d\n", ab, f);
-       }
+        fac[x]=ab;
+	hasil[x]=f;
+	x++;
+      }
 int main(int argc, char** argv)
 {
     int angka[100];
     for (int i = 1; i < argc; ++i)
     	angka[i-1]=atoi(argv[i]);
-    qsort(angka,argc-1,sizeof(int),sort);
     
+    pthread_t tid[100];
     for(int i=0; i < argc-1; i++){
-	pthread_t tid;
-	pthread_create(&tid,NULL,fakt,&angka[i]);
-	pthread_join(tid,NULL);
+	pthread_create(&tid[i],NULL,fakt,&angka[i]);
 
 	}
-	
+    for(int i=0; i < argc-1; i++){
+	pthread_join(tid[i],NULL);
+
+	}
+    qsort(fac,argc-1,sizeof(int),sort);
+    qsort(hasil,argc-1,sizeof(int),sort);
+    for(int i=0; i < argc-1; i++){
+	printf("%d!= %d\n", fac[i], hasil[i]);
+
+	}
 
     return 0;
 }
+
+
